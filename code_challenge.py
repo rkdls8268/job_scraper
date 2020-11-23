569eb5dc00d65bfbea0a30cd948d6eb26afb00b9
rkdls8268 <rkdls8268@gmail.com>
2020-11-23 23:02:25 +0900

    code challenge

---
+++ code_challenge.py
@@ -0,0 +1,32 @@
+# plus, minus, times, division, negation, power, remainder
+
+def plus(a, b):
+    return int(a)+int(b)
+
+def minus(a, b):
+    return int(a)-int(b)
+
+def times(a, b):
+    return int(a)*int(b)
+
+def division(a, b):
+    return int(a)/int(b)
+
+def negation(a):
+    return -int(a)
+
+def power(a, b):
+    return pow(int(a), int(b))
+
+def remainder(a, b):
+    return int(a) % int(b)
+
+plus_result = plus(10, 3)
+minus_result = minus(10, 3)
+times_result = times(10, 3)
+division_result = division(10, 3)
+negation_result = negation(10)
+power_result = power(10, 3)
+remainder_result = remainder(10, 3)
+
+print(plus_result, minus_result, times_result, division_result, negation_result, power_result, remainder_result)
