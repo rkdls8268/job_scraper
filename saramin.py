2b48f2b93db3a686deba4eb0163d55e5052e7b7f
rkdls8268 <rkdls8268@gmail.com>
2020-11-29 22:52:59 +0900

    [실습] extracting links

--- saramin.py
+++ saramin.py
@@ -32,16 +32,19 @@ def extract_job(result):
     location = result.find("div", {"class":"company_info"}).find("p", {"class":"work_place"}).string
     company = str(company)
     title = str(title)
-    return {'title': title, 'company': company, 'location': location}
+    job_id = result["id"][4:]
+    # print(job_id)
+    return {'title': title, 'company': company, 'location': location, "link": f"http://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no&rec_idx={job_id}&recommend_ids=eJxdkMsZwyAMg6fp3W%2FZ5w7C%2FlvU%2BQgh5fjbQpLRLIUHj6j44KuNbqojSA4cdg2KJalieC0UMh6wmOgJw0YYAy32A6dZJtodK6z3mvaWL7zlFZ7uPYgDV7dOb7s%2BZ7onomhhURtSPmFEIeC9le4mT3OylseKqtZW0L5LOEX%2FznR%2B9YZWDZAeeNek%2FtWs6%2FUPzpxT9w%3D%3D&view_type=list&gz=1&t_ref_content=general&t_ref=jobcategory_recruit#seq=0"}
 
 def extract_saramin_jobs(last_page):
     jobs = []
-    # for page in range(last_page):
-    saramin_res = requests.get(f"{URL1}page={0+1}&{URL2}")
-    saramin_soup = BeautifulSoup(saramin_res.text, "html.parser")
-    results = saramin_soup.find_all("div", {"class":"list_item"})
-    # print(results)
-    for result in results:
-        job = extract_job(result)
-        jobs.append(job)
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        saramin_res = requests.get(f"{URL1}page={page+1}&{URL2}")
+        saramin_soup = BeautifulSoup(saramin_res.text, "html.parser")
+        results = saramin_soup.find_all("div", {"class":"list_item"})
+        # print(results)
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
     return jobs
