af8e44187add60703a31c468c9736a458a70292c
rkdls8268 <rkdls8268@gmail.com>
2020-12-09 01:18:18 +0900

    #4 검색어별 결과 출력

--- #4/main.py
+++ #4/main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect
+from saramin import get_jobs
 app = Flask("JobScraper")
 
 # url이랑 주고받고 하려면 다음과 같이 작성해주면 된다.
@@ -10,6 +10,12 @@ def home():
 @app.route("/report")
 def report():
     word = request.args.get('word')
+    if word:
+        word = word.lower()
+        # 입력받은 단어를 get_jobs로 보낸다.
+        jobs = get_jobs(word)
+    else:
+        return redirect("/")
     return render_template("report.html", searching_by=word)
 
 app.run(host="127.0.0.1")
--- #4/saramin.py
+++ #4/saramin.py
@@ -1,11 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL1 = "http://www.saramin.co.kr/zf_user/jobs/list/job-category?"
-URL2 = "cat_key=40430%2C40404%2C40426%2C40427&loc_mcd=101000&loc_cd=102190&searchType=search&searchword=python&panel_type=&search_optional_item=y&search_done=y&panel_count=y"
-
-def extract_saramin_pages():
-    saramin_res = requests.get(URL1+URL2)
+def extract_saramin_pages(url1, url2):
+    saramin_res = requests.get(url1+url2)
     # print(saramin_res.text)
 
     # html parser를 이용해서 html 코드를 Navigate할 것 
@@ -36,15 +33,25 @@ def extract_job(result):
     # print(job_id)
     return {'title': title, 'company': company, 'location': location, "link": f"http://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no&rec_idx={job_id}&recommend_ids=eJxdkMsZwyAMg6fp3W%2FZ5w7C%2FlvU%2BQgh5fjbQpLRLIUHj6j44KuNbqojSA4cdg2KJalieC0UMh6wmOgJw0YYAy32A6dZJtodK6z3mvaWL7zlFZ7uPYgDV7dOb7s%2BZ7onomhhURtSPmFEIeC9le4mT3OylseKqtZW0L5LOEX%2FznR%2B9YZWDZAeeNek%2FtWs6%2FUPzpxT9w%3D%3D&view_type=list&gz=1&t_ref_content=general&t_ref=jobcategory_recruit#seq=0"}
 
-def extract_saramin_jobs(last_page):
+def extract_saramin_jobs(last_page, url1, url2):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping page {page}")
-        saramin_res = requests.get(f"{URL1}page={page+1}&{URL2}")
+        saramin_res = requests.get(f"{url1}page={page+1}&{url2}")
         saramin_soup = BeautifulSoup(saramin_res.text, "html.parser")
         results = saramin_soup.find_all("div", {"class":"list_item"})
         # print(results)
         for result in results:
             job = extract_job(result)
             jobs.append(job)
+    return jobs
+
+# get_jobs는 job을 입력 받아 사용.
+def get_jobs(word):
+    # word를 받아서 url의 인자로 보낼 수 있게 url을 get_jobs 함수 안에 넣어줌
+    url1 = "http://www.saramin.co.kr/zf_user/jobs/list/job-category?"
+    url2 = f"cat_key=40430%2C40404%2C40426%2C40427&loc_mcd=101000&loc_cd=102190&searchType=search&searchword={word}&panel_type=&search_optional_item=y&search_done=y&panel_count=y"
+
+    last_page = extract_saramin_pages(url1, url2)
+    jobs = extract_saramin_jobs(last_page, url1, url2)
     return jobs
