9ab4bf76063e35d843d54f3dff7f10e33aa6f541
rkdls8268 <rkdls8268@gmail.com>
2020-12-10 21:17:59 +0900

    #4 save in db(make it faster)

--- #4/main.py
+++ #4/main.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, redirect
 from saramin import get_jobs
 app = Flask("JobScraper")
 
+db = {}
+
+
 # url이랑 주고받고 하려면 다음과 같이 작성해주면 된다.
 @app.route("/")
 def home():
@@ -12,10 +15,15 @@ def report():
     word = request.args.get('word')
     if word:
         word = word.lower()
-        # 입력받은 단어를 get_jobs로 보낸다.
-        jobs = get_jobs(word)
+        fromDb = db.get(word)
+        if fromDb: # db에 결과가 있으면 scraping 할 필요없이 바로 결과 보여주기
+            jobs = fromDb
+        else:
+            # 입력받은 단어를 get_jobs로 보낸다.
+            jobs = get_jobs(word)
+            db[word] = jobs
     else:
         return redirect("/")
-    return render_template("report.html", searching_by=word)
+    return render_template("report.html", searching_by=word, resultsNumber=len(jobs))
 
 app.run(host="127.0.0.1")
