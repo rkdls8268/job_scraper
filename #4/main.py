d3f9564507f94adb98410a0f8977da206dffb9ef
rkdls8268 <rkdls8268@gmail.com>
2020-12-10 21:40:26 +0900

    #4 rendering jobs

--- #4/main.py
+++ #4/main.py
@@ -24,6 +24,6 @@ def report():
             db[word] = jobs
     else:
         return redirect("/")
-    return render_template("report.html", searching_by=word, resultsNumber=len(jobs))
+    return render_template("report.html", searching_by=word, resultsNumber=len(jobs), jobs=jobs)
 
 app.run(host="127.0.0.1")
